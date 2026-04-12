992e45d6515cbb70eb5f06ea9b0ad189691d1e9a
MrJobsSa <jobssa439@gmail.com>
2026-04-12 20:11:38 +0200

    Fixed auth API URL

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,7 +11,7 @@ function Register() {
   };
 
   const handleSubmit = () => {
-    axios.post("https://sajobs-backend-production.up.railway.app", form)
+    axios.post(axios.post("https://sajobs-backend-production.up.railway.app/api/auth/register", form), form)
       .then((res) => {
         setMessage("Registration successful! You can now login.");
         setError("");
